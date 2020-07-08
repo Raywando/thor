1f7cbe14046542020081bc3f9473bbb756b9e835
Raywando <33800255+Raywando@users.noreply.github.com>
2020-07-08 15:26:11 +0300

    Update thor.sh

--- thor.sh
+++ thor.sh
@@ -1,7 +1,8 @@
+i=10
 while true
 do
 	echo "new IP: $(torsocks curl icanhazip.com --silent)"
-	sleep 10
+	sleep $i
 	service tor reload
 
 	while ! [ $(cat /var/log/syslog | grep 'Anonymizing\|Tor' | tail -1 | grep 'Reloaded Anonymizing overlay network for TCP' | wc -l) == "1" ]
