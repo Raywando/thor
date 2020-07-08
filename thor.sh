175da3ac3e31b541aa7baa0005eea0914331e10c
Raywando <33800255+Raywando@users.noreply.github.com>
2020-07-08 15:24:59 +0300

    Add files via upload

---
+++ thor.sh
@@ -0,0 +1,11 @@
+while true
+do
+	echo "new IP: $(torsocks curl icanhazip.com --silent)"
+	sleep 10
+	service tor reload
+
+	while ! [ $(cat /var/log/syslog | grep 'Anonymizing\|Tor' | tail -1 | grep 'Reloaded Anonymizing overlay network for TCP' | wc -l) == "1" ]
+	do
+		sleep 0.1
+	done
+done
